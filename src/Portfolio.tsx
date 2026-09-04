4ed7131439377cb8a3457d328c2048ef7f6bedc4
ChapaRacer <ethginrod@gmail.com>
2026-09-04 08:30:41 -0600

    Commit 2: Fixed an issue with import

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Portfolio() {
   const [year, setYear] = useState<number>(new Date().getFullYear());
